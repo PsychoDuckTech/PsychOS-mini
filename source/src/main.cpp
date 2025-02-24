1bcd4b69c2c096cfceef98c0dae59318bc78cdf0
GabiBrawl <gabiya219@gmail.com>
2025-02-24 22:35:28 +0000

    Update key mapping for Layer 0 to include function and character keys

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -12,9 +12,9 @@ uint8_t capsLockStatus = 0;
 
 // Key Mapping (Layer 0)
 const uint8_t keyMapL0[totalRows][totalCols] = {
-    {0, 0, 0, 0},
+    {KEY_F5, KEY_B, KEY_C, KEY_D},
     {KEY_NUMLOCK, KEY_SLASH, KEY_KPASTERISK, KEY_MINUS},
-    {KEY_1, KEY_F5, KEY_3, KEY_KPPLUS},
+    {KEY_1, KEY_2, KEY_3, KEY_KPPLUS},
     {KEY_4, KEY_5, KEY_6, 0},
     {KEY_7, KEY_8, KEY_9, KEY_ENTER},
     {KEY_0, 0, KEY_DELETE, 0}};
