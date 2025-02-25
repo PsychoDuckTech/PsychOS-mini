a568317f66d83c3001f9dac6e22aa3716337deea
GabiBrawl <gabiya219@gmail.com>
2025-02-25 21:58:35 +0000

    Although this didnt fix incorrect keys to host, update key mapping for Layer 0 to use keypad symbols---

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -13,11 +13,11 @@ uint8_t capsLockStatus = 0;
 // Key Mapping (Layer 0)
 const uint8_t keyMapL0[totalRows][totalCols] = {
     {KEY_F5, KEY_B, KEY_C, KEY_D},
-    {KEY_NUMLOCK, KEY_SLASH, KEY_KPASTERISK, KEY_MINUS},
-    {KEY_1, KEY_2, KEY_3, KEY_KPPLUS},
-    {KEY_4, KEY_5, KEY_6, 0},
-    {KEY_7, KEY_8, KEY_9, KEY_ENTER},
-    {KEY_0, 0, KEY_DELETE, 0}};
+    {KEY_NUMLOCK, KEY_KPSLASH, KEY_KPASTERISK, KEY_KPMINUS},
+    {KEY_KP1, KEY_KP2, KEY_KP3, KEY_KPPLUS},
+    {KEY_KP4, KEY_KP5, KEY_KP6, 0},
+    {KEY_KP7, KEY_KP8, KEY_KP9, KEY_KPENTER},
+    {KEY_KP0, 0, KEY_KPDOT, 0}};
 
 void setup()
 {
