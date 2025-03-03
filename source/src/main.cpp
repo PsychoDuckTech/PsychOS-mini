f282f1fbb018542ada854a30ccc1e44d352a6407
GabiBrawl <gabiya219@gmail.com>
2025-03-03 22:20:19 +0000

    Update key mapping for Layer 0 to use HID codes and adjust pin assignments... Got the numbers from the keypad working. This is so frustrating, those libraries are ASS

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -2,21 +2,21 @@
 
 const int totalRows = 6, totalCols = 4;
 const int rowPins[totalRows] = {0, 1, 2, 3, 4, 5},
-          colPins[totalCols] = {6, 7, 8, 9},
+          colPins[totalCols] = {6, 7, 9, 10},
           usesMultiplexer = false;
-const int ledPin = 10;
+const int ledPin = 8;
 
 bool moduleConnectionStatus = false;
 uint8_t capsLockStatus = 0;
 
 // Key Mapping (HID codes as hex values)
 const uint8_t keyMapL0[totalRows][totalCols] = {
-    {0x3F, 0x27, 0x06, 0x07}, // KEY_F5, KEY_B, KEY_C, KEY_D (example codes)
-    {0x53, 0x54, 0x55, 0x56}, // KEY_NUMLOCK, KEY_KPSLASH, etc.
-    {0x31, 0x32, 0x33, 0x57}, // KEY_KP1, KEY_KP2, KEY_KP3, KEY_KPPLUS
-    {0x34, 0x35, 0x36, 0x00}, // KEY_KP4, KEY_KP5, KEY_KP6, 0x00 (unused)
-    {0x37, 0x38, 0x39, 0x58}, // KEY_KP7, KEY_KP8, KEY_KP9, KEY_KPENTER
-    {0x40, 0x00, 0x63, 0x00}  // KEY_KP0, 0x00, KEY_KPDOT, 0x00
+    {0x00, 0x00, 0x00, 0x00}, // no keys here for now
+    {0x53, 0x54, 0x55, 0x56}, // HID_KEY_NUM_LOCK, HID_KEY_KEYPAD_DIVIDE, HID_KEY_KEYPAD_MULTIPLY, HID_KEY_KEYPAD_SUBTRACT
+    {0x59, 0x5A, 0x5B, 0x57}, // HID_KEY_KEYPAD_1, HID_KEY_KEYPAD_2, HID_KEY_KEYPAD_3, HID_KEY_KEYPAD_ADD
+    {0x5C, 0x5D, 0x5E, 0x00}, // HID_KEY_KEYPAD_4, HID_KEY_KEYPAD_5, HID_KEY_KEYPAD_6, 0x00 (unused)
+    {0x5F, 0x60, 0x61, 0x58}, // HID_KEY_KEYPAD_7, HID_KEY_KEYPAD_8, HID_KEY_KEYPAD_9, HID_KEY_KEYPAD_ENTER
+    {0x62, 0x00, 0x63, 0x00}  // HID_KEY_KEYPAD_0, 0x00, HID_KEY_KEYPAD_DECIMAL, 0x00
 };
 
 void setup()
